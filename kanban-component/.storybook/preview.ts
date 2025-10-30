8963ecbda793962a0b9e647e1c5ebfd7e5a3a4a0
Rath Bros <SRBros2004@gmail.com>
2025-10-30 16:51:55 +0530

    change kanban-component/.storybook/preview.ts to use vitest setup file

--- kanban-component/.storybook/preview.ts
+++ kanban-component/.storybook/preview.ts
@@ -4,12 +4,14 @@ import '../src/styles/globals.css';
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
+
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
     },
+
     backgrounds: {
       default: 'light',
       values: [
@@ -23,6 +25,13 @@ const preview: Preview = {
         },
       ],
     },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
+    }
   },
 };
 
