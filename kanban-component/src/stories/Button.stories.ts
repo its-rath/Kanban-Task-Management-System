e1ba7acbca79496d482c77a59ac52f9c1f95c0c2
srbros2004 <srbros2004@gmail.com>
2025-10-31 07:23:44 +0000

    all components in working state.

--- kanban-component/src/stories/Button.stories.ts
+++ kanban-component/src/stories/Button.stories.ts
@@ -13,9 +13,7 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  argTypes: {},
 } satisfies Meta<typeof Button>;
 
 export default meta;
--- kanban-component/storybook-static/assets/WithTooltip-V3YHNWJZ-D9zDpw-K.js
+++
@@ -1,1 +0,0 @@
-import{W as h,W as l,a as s}from"./index-Dn9wbvGX.js";import"./iframe-BQC_f_n6.js";import"../sb-preview/runtime.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-aS4jWOJ1.js";import"./index-Cu4lwwaE.js";import"./index-B_TSwO5-.js";import"./index-DrFu-skq.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
